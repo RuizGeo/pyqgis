ec84e63922963a79faa22919f2da6ffff1db424f
Luis Fernando Chimelo Ruiz <ruiz.ch@gmail.com>
2014-08-16 19:05:45 -0300

    Create spatial_filter_ogr_pyqgis.py
    Spatial filter points in pyqgis with OGR

---
+++ spatial_filter_ogr_pyqgis.py
@@ -0,0 +1,29 @@
+from osgeo import ogr
+'''##############'''
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+ini = time.time()
+
+# get the driver
+driver = ogr.GetDriverByName('ESRI Shapefile')
+#Acessar layer ativo em Canvas
+layer = qgis.utils.iface.activeLayer()
+#Obter caminho shape
+pathSHP =layer.dataProvider().dataSourceUri()
+pathSHP = pathSHP.split('|')
+#Ler layer OGR
+shapefile = ogr.Open(str(pathSHP[0]))
+#Obter layer
+layerOGR = shapefile.GetLayer()
+#Iterando sobre a geometria
+features = layer.getFeatures()
+dados=[]
+def get_feature(ft):
+    for i,f in enumerate(ft):
+        featureOGR = layerOGR.GetFeature(i)  
+        #Acessar todos os atributos do ponto
+        featureOGR.items().values()
+        layerOGR.SetSpatialFilter(featureOGR.GetGeometryRef().Buffer(1))
+        d=[layerOGR.GetFeature(i).GetField("z") for j in xrange(layerOGR.GetFeatureCount())]
+        media = sum(d)/len(d)
